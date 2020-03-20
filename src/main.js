7af078fdb5b67b418155f69b25a70dd277c66f59
hhxlz98 <46814315+hhxlz98@users.noreply.github.com>
2020-03-20 18:41:15 +0800

    [add]引入axios、buefy、element-ui、vue-axios。

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(Buefy);
+Vue.use(VueAxios, axios);
 
 /* eslint-disable no-new */
 new Vue({
