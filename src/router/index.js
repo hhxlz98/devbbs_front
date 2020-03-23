b2cc44b2242d37b7f827c081e4c3b7f8811d5d14
hhxlz98 <46814315+hhxlz98@users.noreply.github.com>
2020-03-23 18:51:21 +0800

    [add]添加基本路由，开启history模式。

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/Home'
+import PagaeNotFound from '@/pages/404NotFound.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'Home',
+      path: "/",
+      name: "home",
       component: Home
+    },
+    {
+      path: "*",
+      name: "pageNotFound",
+      component: PagaeNotFound
     }
   ]
 })
