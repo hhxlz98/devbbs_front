0ba2a775ae24464f88599619c2ce8c9524a85e41
hhxlz98 <46814315+hhxlz98@users.noreply.github.com>
2020-03-23 18:45:06 +0800

    [add]添加主页页脚组件

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-center">
+      <p>
+        Created by <strong>赖嘉豪</strong>@
+        <a href="https://www.scut.edu.cn/">SCUT</a>.
+        </br>2020 © 华南理工大学 版权所有
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
