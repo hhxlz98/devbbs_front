4dfc08d307578281725c7170485d4168765ca911
hhxlz98 <46814315+hhxlz98@users.noreply.github.com>
2020-03-27 23:11:52 +0800

    [add]首页广告轮播组件

---
+++ src/components/News.vue
@@ -0,0 +1,55 @@
+<template>
+<b-carousel>
+        <b-carousel-item v-for="(item, i) in addList" :key="i">
+            <section :class="`hero is-small}`">
+                <div class="hero-body has-text-centered">
+                    <span class="image" @click="goAdd(item)">
+                      <img :src='item.src' alt='item.alt' />
+                    </span>
+                </div>
+            </section>
+        </b-carousel-item>
+    </b-carousel>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        addList: [
+          {
+            src:require('../assets/adds/pic1.jpg'),
+            alt:'',
+            url:'https://www.baidu.com'
+          },
+          {
+            src:require('../assets/adds/pic2.jpg'),
+            alt:'',
+            url:'https://www.baidu.com'
+          },
+          {
+            src:require('../assets/adds/pic3.jpg'),
+            alt:'',
+            url:'https://www.baidu.com'
+          },
+          {
+            src:require('../assets/adds/pic4.jpg'),
+            alt:'',
+            url:'https://www.baidu.com'
+          },
+        ]
+      }
+    },
+    methods: {
+      getImgUrl(value) {
+        return `https://picsum.photos/id/43${value}/576/400`;
+      },
+      goAdd(item) {
+        window.open(item.url,'_blank')
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
