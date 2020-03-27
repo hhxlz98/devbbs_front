1237011c08289a6f79f768b1440f3c827ccd6594
hhxlz98 <1154005450@qq.co>
2020-03-27 23:28:05 +0800

    [add]主页添加新组件

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="home">
   <TopNav />
+  <Tiles />
   <Footer />
   </div>
 </template>
 
 <script>
-import TopNav from "../components/TopNav.vue";
+import TopNav from "../components/TopNav.vue"
 import Footer from "../components/Footer.vue"
+import News from "../components/News.vue"
+import Tiles from "../components/Tiles.vue"
 
 export default {
   components: {
     TopNav,
     Footer,
+    News,
+    Tiles,
   }
 };
 </script>
