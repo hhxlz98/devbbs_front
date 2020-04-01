57f41ed414a86655cf36cda3f5c1e17027f44dcc
hhxlz98 <1154005450@qq.co>
2020-04-01 20:43:35 +0800

    [add]增加全部板块、我的板块路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/Home'
 import Register from '@/pages/Register.vue'
+import AllPate from '@/pages/AllPlate.vue'
 import PagaeNotFound from '@/pages/404NotFound.vue'
+import MyPlate from '@/pages/MyPlate.vue'
 
 Vue.use(Router)
 
@@ -20,9 +22,19 @@ export default new Router({
       component: Register,
     },
     {
+      path:"/allPlate",
+      name:"allPlate",
+      component: AllPate,
+    },
+    {
+      path:"/myPlate",
+      name:"myPlate",
+      component: MyPlate,
+    },
+    {
       path: "*",
       name: "pageNotFound",
-      component: PagaeNotFound
+      component: PagaeNotFound,
     }
   ]
 })
