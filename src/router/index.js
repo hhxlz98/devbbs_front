28d5337bf60b76664d2ca6bfe86370e474c1c359
hhxlz98 <1154005450@qq.com>
2020-04-08 16:33:08 +0800

    [add]增加板块内容、帖子详细、发表帖子、用户主页路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,10 @@ import Register from '@/pages/Register.vue'
 import AllPate from '@/pages/AllPlate.vue'
 import PagaeNotFound from '@/pages/404NotFound.vue'
 import MyPlate from '@/pages/MyPlate.vue'
+import PlateContent from '@/pages/PlateContent.vue'
+import PostHome from '@/pages/PostHome.vue'
+import PublishPost from '@/pages/PublishPost.vue'
+import UserInfo from '@/pages/UserInfo.vue'
 
 Vue.use(Router)
 
@@ -30,6 +34,27 @@ export default new Router({
       path:"/myPlate",
       name:"myPlate",
       component: MyPlate,
+    },
+    {
+      path:"/plate/:plateId&plateName=:plateName",
+      name:"plateContent",
+      component: PlateContent,
+
+    },
+    {
+      path:"/:postId&author=:name",
+      name:"postContent",
+      component: PostHome,
+    },
+    {
+      path:"/publishPostFor:type",
+      name:"publishPost",
+      component: PublishPost,
+    },
+    {
+      path:"/:userId&userName=:userName",
+      name:"userInfo",
+      component: UserInfo,
     },
     {
       path: "*",
