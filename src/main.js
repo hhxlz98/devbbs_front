95aae5ea500f815d1c2a45f4fffc38ff32fcd3bb
hhxlz98 <1154005450@qq.com>
2020-04-08 15:58:42 +0800

    [add]增加时间处理等过滤器

--- src/main.js
+++ src/main.js
@@ -46,6 +46,78 @@ Vue.filter('stringLengthConversion',function(str,len){
   }
 })
 
+Vue.filter('tabFormatter', function(str){
+  if(str == 'ask')
+    return '问答'
+  else if(str == 'share')
+    return '分享'
+   else
+    return '未知'
+})
+
+Vue.filter('timeFormatterToLast',function(dateVal){
+ if (!dateVal) return ''
+ var time = new Date().getTime() - dateVal;
+ if (time < 0) {
+   return ''
+ }  else if ((time / 1000 < 30)) {
+    return '刚刚'
+  } else if (time / 1000 < 60) {
+    return parseInt((time / 1000)) + '秒前'
+  } else if ((time / 60000) < 60) {
+    return parseInt((time / 60000)) + '分钟前'
+  } else if ((time / 3600000) < 24) {
+    return parseInt(time / 3600000) + '小时前'
+  } else if ((time / 86400000) < 31) {
+    return parseInt(time / 86400000) + '天前'
+  } else if ((time / 2592000000) < 12) {
+    return parseInt(time / 2592000000) + '个月前'
+  } else {
+    return parseInt(time / 31536000000) + '年前'
+  }
+})
+
+Vue.filter('timeFormatterToString',function(dateVal){ //只精确到分钟
+  var date = new Date(dateVal)
+  var YY = date.getFullYear() + '-'
+  var MM = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-'
+  var DD = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate())
+  var hh = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':'
+  var mm = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':'
+  var ss = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds())
+  return YY + MM + DD + " " + hh + mm + ss;
+})
+
+Vue.filter('numberFormatter',function(value){
+  var digit = -1;
+  var number = value;
+  while (number >= 1) {
+    digit++;
+    number = number / 10;
+  }
+  if (digit < 3) {
+    return value;
+  } else if (digit >= 3 && digit < 6) {
+    var sNum = value / 1000;
+    var nNum = parseFloat(sNum);
+    if(!isNaN(nNum)) {
+    	return nNum.toFixed(1) + "k";
+    }
+  } else if (digit >= 6 && digit < 9) {
+    var sNum = value / 10000;
+    var nNum = parseFloat(sNum);
+    if(!isNaN(nNum)) {
+    	return nNum.toFixed(1) + "w";
+    }
+  }else {
+      var sNum = value / 10000000;
+      var nNum = parseFloat(sNum);
+      if(!isNaN(nNum)) {
+      	return nNum.toFixed(1) + "kw";
+    }
+  }
+})
+
 new Vue({
   el: '#app',
   router,
