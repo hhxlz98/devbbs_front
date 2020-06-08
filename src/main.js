77055a9d19c2508a70eb3439da57def0d5df15a6
hhxlz98 <1154005450@qq.com>
2020-06-08 14:29:10 +0800

    [update]main.js
    添加众多的全局过滤器以及公共函数

--- src/main.js
+++ src/main.js
@@ -10,34 +10,36 @@ import Buefy from "buefy";
 import "buefy/dist/buefy.css";
 import axios from "axios";
 import VueAxios from "vue-axios";
-
+import { MessageBox } from 'element-ui';
 import "./assets/css/index.css"
 
-import commonFunction from "./utils/index.js"
-Vue.prototype.commonFunction = commonFunction
+import CommonUtil from "./utils/index.js"
+Vue.prototype.CommonUtil = CommonUtil
+
+Vue.prototype.$filters = Vue.options.filters
 
 Vue.config.productionTip = true
 Vue.use(ElementUI);
 Vue.use(Buefy);
 Vue.use(VueAxios, axios);
 
-
-
 /* eslint-disable no-new */
 
 Vue.filter('stringLengthConversion',function(str,len){
   var result = "";
   var strlen = 0;
-  for(var i = 0;i < str.length; i++){
-      if(str.charCodeAt(i) > 255){
-        strlen += 2; //如果是汉字，则字符串长度加2
-      } else {
-        strlen++;
-      }
-      result += str.substr(i,1);
-      if(strlen >= len){
-          break;
-      }
+  if (str) {
+    for(var i = 0;i < str.length; i++){
+        if(str.charCodeAt(i) > 255){
+          strlen += 2; //如果是汉字，则字符串长度加2
+        } else {
+          strlen++;
+        }
+        result += str.substr(i,1);
+        if(strlen >= len){
+            break;
+        }
+    }
   }
   if(strlen < len){
     return result
@@ -47,19 +49,19 @@ Vue.filter('stringLengthConversion',function(str,len){
 })
 
 Vue.filter('tabFormatter', function(str){
-  if(str == 'ask')
+  if(str == 'Ask')
     return '问答'
-  else if(str == 'share')
+  else if(str == 'Share')
     return '分享'
    else
     return '未知'
 })
 
 Vue.filter('timeFormatterToLast',function(dateVal){
- if (!dateVal) return ''
+ if (!dateVal) return '未知'
  var time = new Date().getTime() - dateVal;
  if (time < 0) {
-   return ''
+   return '---'
  }  else if ((time / 1000 < 30)) {
     return '刚刚'
   } else if (time / 1000 < 60) {
@@ -77,7 +79,29 @@ Vue.filter('timeFormatterToLast',function(dateVal){
   }
 })
 
-Vue.filter('timeFormatterToString',function(dateVal){ //只精确到分钟
+Vue.filter('timeFormatterToRest',function(dateVal){
+ if (isNaN(dateVal)) return '未知'
+  if (dateVal <= 0){
+    return '---'
+  } else if (dateVal / 1000 < 60) {
+    return parseInt((dateVal / 1000)) + '秒'
+  } else if ((dateVal / 60000) < 60) {
+    return parseInt((dateVal / 60000)) + '分钟'
+  } else if ((dateVal / 3600000) < 24) {
+    var hours = parseInt(dateVal / 3600000);
+    var minutes = parseInt((dateVal - hours * 3600000) / 60000);
+    return hours + '小时' + minutes + '分钟'
+  } else {
+    var days = parseInt(dateVal / 86400000);
+    var hours = parseInt((dateVal - days * 86400000) / 3600000);
+    return days + '天' + hours + '小时'
+  }
+
+})
+
+
+
+Vue.filter('timeFormatterToString',function(dateVal){
   var date = new Date(dateVal)
   var YY = date.getFullYear() + '-'
   var MM = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-'
@@ -86,6 +110,31 @@ Vue.filter('timeFormatterToString',function(dateVal){ //只精确到分钟
   var mm = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':'
   var ss = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds())
   return YY + MM + DD + " " + hh + mm + ss;
+})
+
+Vue.filter('timeFormatterToStringForDay',function(dateVal){
+  var date = new Date(dateVal)
+  var YY = date.getFullYear() + '-'
+  var MM = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-'
+  var DD = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate())
+  return YY + MM + DD;
+})
+
+Vue.filter('timeFormatterToStringForMinute', function(dateVal){
+  var date = new Date(dateVal)
+  var YY = date.getFullYear()
+  var thisDate = new Date();
+  var thisYY = thisDate.getFullYear()
+  if (YY == thisYY) {
+    YY = ''
+  } else {
+    YY = YY + '-'
+  }
+  var MM = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-'
+  var DD = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate())
+  var hh = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':'
+  var mm = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+  return YY + MM + DD + " " + hh + mm;
 })
 
 Vue.filter('numberFormatter',function(value){
@@ -117,6 +166,20 @@ Vue.filter('numberFormatter',function(value){
     }
   }
 })
+
+Vue.filter('sexTransfrom', function(value){
+  if(value == '-') {
+    return "秘密"
+  } else if (value == 'man') {
+    return '男'
+  } else if (value == 'woman') {
+    return '女'
+  } else {
+    return '未知'
+  }
+})
+
+
 
 new Vue({
   el: '#app',
