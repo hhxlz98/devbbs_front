8329206ae8469e3c7b77a15eabf55cb073c3a646
hhxlz98 <1154005450@qq.com>
2020-06-08 14:25:33 +0800

    [update] 悬赏信息组件
    更新props传值，更改参数名

--- src/components/Reward.vue
+++ src/components/Reward.vue
@@ -1,19 +1,19 @@
 <template>
   <span class="reward_body">
-     <div v-if="!isOver">
-       <span>悬赏积分：{{figure}}</span>
+     <div v-if="rewardInfo.status == 0" class="unsolve">
+       <span>悬赏积分：{{rewardInfo.reward}}</span>
        <el-divider direction="vertical"></el-divider>
-       <span>热心助人：{{helpful}}</span>
+       <span>热心助人：{{rewardInfo.helpfulNumber * rewardInfo.helpfulReward}}</span>
        <el-divider direction="vertical"></el-divider>
-       <i class="el-icon-warning-outline">剩余时间：{{restTime}}</i>
+       <i class="el-icon-warning-outline">剩余时间：{{rewardInfo.restTime | timeFormatterToRest}}</i>
      </div>
-     <div v-else-if="isSolve" :class="[isSolve ? 'solve': 'loose']">
-       <span>悬赏积分：{{figure}}</span>
+     <div v-else-if="rewardInfo.status == 1" class="solve">
+       <span>悬赏积分：{{rewardInfo.reward}}</span>
        <el-divider direction="vertical"></el-divider>
        <i class="el-icon-circle-check">已解决</i>
      </div>
-     <div v-else :class="[isSolve ? 'solve': 'loose']">
-       <span>悬赏积分：{{figure}}</span>
+     <div v-else-if="rewardInfo.status == 2" class="loose">
+       <span>悬赏积分：{{rewardInfo.reward}}</span>
        <el-divider direction="vertical"></el-divider>
        <i class="el-icon-circle-close">已失效</i>
      </div>
@@ -24,32 +24,37 @@
   export default {
     data() {
       return {
-        figure: 50,
-        helpful: 20,
-        isOver: false,
-        restTime: "7天6小时",
-        isSolve: false,
+
       }
-    }
+    },
+    props:['rewardInfo'],
   }
 </script>
 
 <style scoped>
   .reward_body {
-    border-radius: 30px;
-    background-color: #e4f3ff;
     display:table;
     height: 30px;
     line-height: 30px;
     font-size: 12px;
-    padding: 0 6px 0 6px;
   }
 
+  .unsolve {
+    padding: 0 6px 0 6px;
+    border-radius: 30px;
+    background-color: #e4f3ff;
+   }
+
   .solve {
+    padding: 0 6px 0 6px;
+    border-radius: 30px;
     background-color: #a6e47f;
+
   }
 
   .loose {
+    padding: 0 6px 0 6px;
+    border-radius: 30px;
     background-color: #ffaa7f;
   }
 
