b1fde4c6dad24af0393061e4d549fba6fde95eed
hhxlz98 <1154005450@qq.com>
2020-06-08 12:21:27 +0800

    [add] 增加后台管理页面布局组件

---
+++ src/components/backManange/MainLayout.vue
@@ -0,0 +1,56 @@
+<template>
+  <el-row class="layout_container">
+    <el-col :span="24"><router-view name="top"></router-view></el-col>
+    <el-col :span="24" class="main">
+      <router-view name="aside"></router-view>
+
+      <section class="content-container">
+        <div class="grid-content bg-purple-light">
+          <transition name="fade" mode="out-in">
+            <router-view></router-view>
+          </transition>
+        </div>
+      </section>
+    </el-col>
+    <BackFooter></BackFooter>
+  </el-row>
+</template>
+
+<script>
+  import BackFooter from './BackFooter.vue'
+  export default{
+    components: {
+      BackFooter,
+    },
+  }
+</script>
+
+<style>
+  .layout_container {
+    position: absolute;
+    top: 0px;
+    bottom: 0px;
+    width: 100%;
+  }
+  .layout_container .main {
+    display: -ms-flexbox;
+    display: flex;
+    position: absolute;
+    top: 50px;
+    bottom: 45px;
+    overflow: hidden;
+  }
+  .layout_container .content-container {
+    background: #f2f2f2;
+    -ms-flex: 1;
+    flex: 1;
+    overflow-y: auto;
+    padding: 0px;
+    height: 100%;
+  }
+  .grid-content {
+    border-radius: 2px;
+    background-color: #fff;
+
+  }
+</style>
