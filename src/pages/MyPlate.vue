8c9cf4090b29a1a6772c5cf6cc4fdab50020463c
hhxlz98 <1154005450@qq.com>
2020-06-08 14:33:05 +0800

    [update] 我的版块
    优化无版块时的状态，添加后端请求

--- src/pages/MyPlate.vue
+++ src/pages/MyPlate.vue
@@ -6,23 +6,25 @@
       <div class="box topMargin">
         <p class="headTitle">我的关注</p>
 
-          <el-row :gutter="20">
+          <el-row :gutter="20" v-if="myPlateList.length > 0">
             <el-col v-for="(plate,index) in myPlateList" :key="plate.id" :span="4" class="topMargin">
               <Plate   :plate-info="plate"></Plate>
             </el-col>
           </el-row>
 
+          <p v-else class="no_myplate">暂无关注板块！快去关注吧</p>
+
       </div>
 
       <div v-if="managePlateList.length > 0" class="box">
         <p class="headTitle">我的管理</p>
-        
+
           <el-row :gutter="20">
             <el-col v-for="(plate,index) in managePlateList" :key="plate.id" :span="4" class="topMargin">
               <Plate   :plate-info="plate"></Plate>
             </el-col>
           </el-row>
-        
+
       </div>
       <Footer />
     </div>
@@ -30,72 +32,42 @@
 </template>
 
 <script>
+  import { getMyPlateList } from '@/api'
   import TopNav from "../components/TopNav.vue"
   import Footer from "../components/Footer.vue"
   import Plate from "../components/Plate.vue"
   export default {
     data() {
       return {
-        myPlateList:[
-          {
-           id: 1,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言adfafjalsjfasjfal;sd",
-           isFollow: true,
-          },
-          {
-           id: 2,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 3,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 4,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 5,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 6,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 7,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 8,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-        ],
-        managePlateList:[
-          {
-           id: 1,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
+        myPlateList:[],
 
-        ],
+        managePlateList:[],
       }
+    },
+    methods: {
+
+    },
+    mounted() {
+      var type = 0;
+      getMyPlateList(this.$store.state.user.userId, type).then(
+      response => {
+        const data = response.data;
+
+        if(data.code == "201") {
+          this.myPlateList = data.info;
+          console.log(this.myPlateList)
+        }
+      });
+      type = 1;
+      getMyPlateList(this.$store.state.user.userId, type).then(
+      response => {
+        const data = response.data;
+      
+        if(data.code == "201") {
+          this.managePlateList = data.info;
+          console.log(this.managePlateList)
+        }
+      });
     },
     components: {
         Plate,
@@ -105,8 +77,13 @@
   }
 </script>
 
-<style>
-  .topMargin{
+<style scoped lang="scss">
+  .topMargin {
     margin-top: 1rem;
+  }
+  .no_myplate {
+    margin: 6px 0 0 6px;
+    color: $only-read-color;
+    font-size: 16px;
   }
 </style>
