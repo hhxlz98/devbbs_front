737588b3233fe6ef285ed427bd26c786091f6dc7
hhxlz98 <1154005450@qq.com>
2020-06-08 12:38:58 +0800

    [add] 我的信息——发件箱

---
+++ src/components/message/SentMail.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-tabs>
+    <el-tab-pane>
+      <span slot="label">已发站内信</span>
+        <SendedMail  ref="SendedMail"></SendedMail>
+      </el-tab-pane>
+    <el-tab-pane>
+      <span slot="label">已发好友申请</span>
+        <SendedFriendApply></SendedFriendApply>
+      </el-tab-pane>
+    <el-tab-pane>
+      <span slot="label">发送站内信</span>
+        <SendMail @send-mail-success="sendMailSuccess"></SendMail>
+      </el-tab-pane>
+  </el-tabs>
+</template>
+
+<script>
+  import SendedMail from './SendedMail.vue'
+  import SendedFriendApply from './SendedFriendApply.vue'
+  import SendMail from './SendMail.vue'
+
+  export default {
+    methods: {
+      sendMailSuccess() {
+        console.log("ressss");
+        this.$refs.SendedMail.reRequestMail()
+      },
+    },
+    components:{
+      SendedMail,
+      SendedFriendApply,
+      SendMail,
+    },
+  }
+</script>
+
+<style>
+</style>
