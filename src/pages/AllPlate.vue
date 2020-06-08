6b32b023be6ae13b47566e3acc95b35c2a9fdbd0
hhxlz98 <1154005450@qq.com>
2020-06-08 14:31:23 +0800

    [update] 全部版块列表
    添加后端请求，优化没有版块时的状态

--- src/pages/AllPlate.vue
+++ src/pages/AllPlate.vue
@@ -3,15 +3,15 @@
     <div class="container">
       <TopNav />
 
-      <div class="box topMargin">
+      <div class="box topMargin" v-if="allPlateList.length>0">
         <p class="headTitle">全部板块</p>
           <el-row :gutter="20">
-            <el-col v-for="(plate,index) in allPlateList" :key="plate.id" :span="4" class="topMargin">
-              <Plate   :plate-info="plate"></Plate>
+            <el-col v-for="(plate,index) in allPlateList" :key="plate.plate_id" :span="4" class="topMargin">
+              <Plate  :plate-info="plate"></Plate>
             </el-col>
           </el-row>
       </div>
-      
+
       <Footer />
     </div>
   </div>
@@ -19,63 +19,31 @@
 </template>
 
 <script>
+  import { getAllPlateList } from '@/api'
   import TopNav from "../components/TopNav.vue"
   import Footer from "../components/Footer.vue"
   import Plate from "../components/Plate.vue"
   export default {
     data() {
       return {
-        allPlateList:[
-          {
-           id: 1,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言一种前端语言一种前端语言一种前端语言一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 2,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 3,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 4,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 5,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 6,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 7,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-          {
-           id: 8,
-           typeName:"Javascript",
-           typeDesc:"一种前端语言",
-           isFollow: true,
-          },
-        ]
+        allPlateList:[],
       }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+      getAllPlateList(this.$store.state.user.userId).then(
+      response => {
+        const data = response.data;
+
+        if(data.code == "201") {
+          this.allPlateList = data.info;
+          console.log(this.allPlateList)
+        }
+      })
+
     },
     components: {
       Plate,
