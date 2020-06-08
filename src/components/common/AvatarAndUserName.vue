9b2c12d476aaf67619027ca9fa8ba94cf123900f
hhxlz98 <1154005450@qq.com>
2020-06-08 12:27:14 +0800

    [add] 公共组件
    增加带链接的用户名和带链接和头像的用户名

---
+++ src/components/common/AvatarAndUserName.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="position_container" @click.stop="toUserInfo">
+    <el-avatar :size="size" fit="fill" :src="CommonUtil.baseUrl + url"></el-avatar>
+    <div class="userN">{{userName}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    methods: {
+      toUserInfo() {
+        let routeUrl = this.$router.resolve({
+             name: "userInfo",
+             params:{userId: this.userId, userName: this.userName}
+           });
+        window.open(routeUrl.href, '_blank');
+      },
+
+    },
+    props:['size', 'url', 'userName', 'userId'],
+  }
+</script>
+
+<style scoped>
+.position_container {
+  /* border: 1px solid #000000; */
+  display: table;
+}
+
+.position_container :hover{
+  cursor: pointer;
+}
+
+.userN {
+  display: table-cell;
+  vertical-align: middle;
+}
+</style>
---
+++ src/components/common/LinkUserName.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="userName" @click.stop="toUserInfo">{{userName}}</span>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    methods: {
+      toUserInfo() {
+        let routeUrl = this.$router.resolve({
+             name: "userInfo",
+             params:{userId: this.userId, userName: this.userName}
+           });
+        window.open(routeUrl.href, '_blank');
+      },
+    },
+    props:['userName', 'userId'],
+  }
+</script>
+
+<style scoped>
+  .userName {
+    color: #55ff7f;
+  }
+  .userName:hover {
+    color: #0000FF;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
