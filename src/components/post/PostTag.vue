e1d228b49606bbd51fdb9a13320d7c09fa548316
hhxlz98 <1154005450@qq.com>
2020-06-08 12:41:43 +0800

    [add] 帖子tag
    讲帖子tag移出作为组件使用

---
+++ src/components/post/PostTag.vue
@@ -0,0 +1,39 @@
+<template>
+  <span>
+    <span v-if="putTop" class="putTop">置顶</span>
+    <span v-if="putGood" class="putGood">精华</span>
+    <span class="putNormal"> {{ tab | tabFormatter }} </span>
+  </span>
+</template>
+
+<script>
+  export default {
+    props:['putTop', 'putGood', 'tab'],
+  }
+</script>
+
+<style scoped>
+  .putGood,.putTop {
+    background: #80bd01;
+    padding: 2px 2px 2px 2px;
+    border-radius: 3px;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    -o-border-radius: 3px;
+    color: #fff;
+    font-size: 12px;
+    margin-right: 3px;
+    margin-bottom: 1px;
+  }
+  .putNormal {
+    background-color: #e5e5e5;
+    color: #999;
+    padding: 2px 1px 2px 3px;
+    border-radius: 3px;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    -o-border-radius: 3px;
+    font-size: 12px;
+    margin-right: 7px;
+  }
+</style>
