4eadd919411ec6d4cc38522a0e4f9c1da075be5f
hhxlz98 <1154005450@qq.com>
2020-06-08 14:37:21 +0800

    [update] 更新路由
    增加我的信息模块、社区任务模块、后台模块的路由

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,25 @@ import PlateContent from '@/pages/PlateContent.vue'
 import PostHome from '@/pages/PostHome.vue'
 import PublishPost from '@/pages/PublishPost.vue'
 import UserInfo from '@/pages/UserInfo.vue'
+import MyMessage from '@/pages/MyMessage.vue'
+import Task from '@/pages/Task.vue'
+import SearchPost from '@/pages/SearchPost.vue'
+import Transition from '@/pages/Transition.vue'
+
+import BackLogin from '@/pages/BackLogin.vue'
+import BackMainLayout from '../components/backManange/MainLayout.vue'
+import BackTopBar from '../components/backManange/TopBar.vue'
+import BackLeftNav from '../components/backManange/LeftNav.vue'
+import BackContent from '../components/backManange/Content.vue'
+import BackPlateList from '../components/backManange/plate/PlateList.vue'
+import BackAdminManage from '../components/backManange/plate/AdminManage.vue'
+import BackAddPlate from '../components/backManange/plate/AddPlate.vue'
+import BackQueryUser from '../components/backManange/user/QueryUser.vue'
+import BackUserProhibit from '../components/backManange/user/UserProhibit.vue'
+import BackUserReportList from '../components/backManange/user/UserReportList.vue'
+import BackNewsManage from '../components/backManange/home/NewsManage.vue'
+
+import Test from '@/pages/Test.vue'
 
 Vue.use(Router)
 
@@ -26,12 +45,12 @@ export default new Router({
       component: Register,
     },
     {
-      path:"/allPlate",
+      path:"/plate/all",
       name:"allPlate",
       component: AllPate,
     },
     {
-      path:"/myPlate",
+      path:"/plate/my",
       name:"myPlate",
       component: MyPlate,
     },
@@ -42,19 +61,106 @@ export default new Router({
 
     },
     {
-      path:"/:postId&author=:name",
+      path:"/post/:postId&name=:name",
       name:"postContent",
       component: PostHome,
     },
     {
-      path:"/publishPostFor:type",
+      path:"/publish:type&PlateId:plateId",
       name:"publishPost",
       component: PublishPost,
     },
     {
-      path:"/:userId&userName=:userName",
+      path:"/serachkey=:key&type=:type",
+      name:"searchPost",
+      component: SearchPost,
+    },
+    {
+      path:"/userInfo/:userId&userName=:userName",
       name:"userInfo",
       component: UserInfo,
+    },
+    {
+      path:"/myMessage/:userId&userName=:userName",
+      name:"myMessage",
+      component: MyMessage,
+    },
+    {
+      path:"/transition",
+      name:"transition",
+      component: Transition,
+    },
+    {
+      path:"/forumTask/:userId&userName=:userName",
+      name:"forumTask",
+      component: Task,
+    },
+    {
+      path:"/backLogin",
+      name: "backLogin",
+      component: BackLogin,
+    },
+    {
+      path:"/backManage",
+      name:"backManage",
+      component: BackMainLayout,
+      menuShow: true,
+      redirect: '/backManage/plate/list',
+      children: [
+        {
+        path: '/backManage/plate',
+        name: '板块管理',
+        icon: 'fa fa-th-large',
+        menuShow: true,
+        components: {
+          top: BackTopBar,
+          aside: BackLeftNav,
+          default: BackContent,
+        },
+        children: [
+          {path: '/backManage/plate/list', name: '板块列表', component: BackPlateList, menuShow: true },
+          {path: '/backManage/plate/add', name: '添加板块', component: BackAddPlate, menuShow:true },
+          {path: '/backManage/plate/adminManage', name: '版主管理', component: BackAdminManage, menuShow: true }
+        ]
+      },
+      {
+        path: '/backManage/user',
+        name: '用户管理',
+        icon: 'fa fa-user-circle',
+        menuShow: true,
+        components: {
+          top: BackTopBar,
+          aside: BackLeftNav,
+          default: BackContent,
+        },
+        children: [
+          {path: '/backManage/user/query', name: '用户查询', component: BackQueryUser, menuShow: true},
+          {path: '/backManage/user/prohibit', name: '用户封禁', component: BackUserProhibit, menuShow: true},
+          {path: '/backManage/user/report', name: '用户举报', component: BackUserReportList, menuShow: true}
+        ]
+      },
+      {
+        path: '/backManage/home',
+        name: '首页管理',
+        icon: 'fa fa-newspaper-o',
+        menuShow: true,
+        components: {
+          top: BackTopBar,
+          aside: BackLeftNav,
+          default: BackContent,
+        },
+        children: [
+          {path: '/backManage/home/news', name: '广告管理', component: BackNewsManage, menuShow: true},
+        ]
+      },
+      ]
+
+
+    },
+    {
+      path:"/test",
+      name:"test",
+      component: Test,
     },
     {
       path: "*",
