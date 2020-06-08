8c1e86195dbba99522853f8886df2ad1feac289e
hhxlz98 <1154005450@qq.com>
2020-06-08 12:32:55 +0800

    [update] 主页广告
    移除静态广告，改为从后端请求数据

--- src/components/home/News.vue
+++ src/components/home/News.vue
@@ -1,10 +1,10 @@
 <template>
-<b-carousel>
+<b-carousel class="news_container">
         <b-carousel-item v-for="(item, i) in addList" :key="i">
             <section :class="`hero is-small}`">
                 <div class="hero-body has-text-centered">
                     <span class="image" @click="goAdd(item)">
-                      <img :src='item.src' alt='item.alt' />
+                      <img :src='CommonUtil.baseUrl + item.imgUrl' :alt='item.newsIntro' />
                     </span>
                 </div>
             </section>
@@ -13,43 +13,56 @@
 </template>
 
 <script>
+  import { getHomeNewsList } from '@/api'
   export default {
     data() {
       return {
         addList: [
-          {
-            src:require('@/assets/adds/pic1.jpg'),
-            alt:'',
-            url:'https://www.baidu.com'
-          },
-          {
-            src:require('@/assets/adds/pic2.jpg'),
-            alt:'',
-            url:'https://www.baidu.com'
-          },
-          {
-            src:require('@/assets/adds/pic3.jpg'),
-            alt:'',
-            url:'https://www.baidu.com'
-          },
-          {
-            src:require('@/assets/adds/pic4.jpg'),
-            alt:'',
-            url:'https://www.baidu.com'
-          },
+          // {
+          //   src:require('@/assets/adds/pic1.jpg'),
+          //   alt:'',
+          //   url:'https://www.baidu.com'
+          // },
+          // {
+          //   src:require('@/assets/adds/pic2.jpg'),
+          //   alt:'',
+          //   url:'https://www.baidu.com'
+          // },
+          // {
+          //   src:require('@/assets/adds/pic3.jpg'),
+          //   alt:'',
+          //   url:'https://www.baidu.com'
+          // },
+          // {
+          //   src:require('@/assets/adds/pic4.jpg'),
+          //   alt:'',
+          //   url:'https://www.baidu.com'
+          // },
         ]
       }
     },
     methods: {
-      getImgUrl(value) {
-        return `https://picsum.photos/id/43${value}/576/400`;
-      },
       goAdd(item) {
-        window.open(item.url,'_blank')
+        window.open(item.toLink,'_blank')
+      },
+      getNews() {
+        getHomeNewsList().then(response => {
+          const data = response.data;
+          console.log(data);
+          if (data.code == '1205') {
+            this.addList = data.info;
+          }
+        })
       }
+    },
+    mounted() {
+      this.getNews()
     }
   }
 </script>
 
-<style>
+<style scoped>
+  .news_container :hover{
+    cursor: pointer;
+  }
 </style>
